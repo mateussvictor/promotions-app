af3dfe77b676b32c7f3647e6913d609edfaa1c33
Mateus Victor <mateus.svictor@gmail.com>
2021-09-17 19:29:31 -0300

    feat: add Card component

---
+++ web/src/components/Promotion/Card/Card.jsx
@@ -0,0 +1,42 @@
+import './Card.scss'
+
+const PromotionCard = ({ promotion }) => {
+  return (
+    <div className="card__container">
+      <img
+        src={promotion.imageUrl}
+        alt="Card with the price and with the link to buy the product"
+        className="card__img"
+      />
+
+      <div className="card__info">
+        <h1 className="card__title">{promotion.title}</h1>
+        <span className="card__price">R$ {promotion.price}</span>
+
+        <footer className="card__footer">
+          {promotion.comments.length > 0 && (
+            <div className="card__comment">
+              &quot;{promotion.comments[0].comment}&quot;
+            </div>
+          )}
+
+          <div className="card__comments-count">
+            {promotion.comments.length}{' '}
+            {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}
+          </div>
+
+          <a
+            href={promotion.url}
+            target="_blank"
+            rel="noreferrer"
+            className="card__link"
+          >
+            IR PARA O SITE
+          </a>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export { PromotionCard }
